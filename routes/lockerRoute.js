a8d8e5b2bac26054753d4d0dfaced417db04cf72
Dinesh Kumar <mdineshdk21@gmail.com>
2022-02-09 16:20:42 +0530

    unlock update

--- routes/lockerRoute.js
+++ routes/lockerRoute.js
@@ -1,8 +1,9 @@
 const route = require('express').Router();
 const service = require("../services/locker.services");
 
-route.post("/insertlocker",service.newLocker);
-route.post("/unlock",service.unlock);
-route.get("/lockerdata",service.lockerdata);
+route.post("/insertlocker", service.newLocker);
+route.post("/unlock", service.unlock);
+route.get("/lockerdata", service.lockerdata);
+route.post("/unlockupdate", service.Updatedata)
 
 module.exports = route;
--- services/locker.services.js
+++ services/locker.services.js
@@ -1,67 +1,78 @@
 var mqtt = require('mqtt');
-const db =require("../mongo");
+const db = require("../mongo");
 
 
 const service = {
-    async unlock(req,res) {
-        const subscribe_topic = req.body.subscribe_topic;
-        const publish_topic = req.body.publish_topic;
-        const key = req.body.key
-        try {
-            var client = mqtt.connect('mqtt://broker.emqx.io:1883', {
-            username:'emqx',
-            password:"public"
-        });
+  async unlock(req, res) {
+    const subscribe_topic = req.body.subscribe_topic;
+    const publish_topic = req.body.publish_topic;
+    const key = req.body.key
+    try {
+      var client = mqtt.connect('mqtt://broker.emqx.io:1883', {
+        username: 'emqx',
+        password: "public"
+      });
 
-            var ssd1406topic = publish_topic;
-            var ssd1306topic = subscribe_topic;
-            
-            console.log(ssd1406topic);
-            client.on('connect',function(){
-                console.log('connected');
+      var ssd1406topic = publish_topic;
+      var ssd1306topic = subscribe_topic;
 
-                client.subscribe(ssd1306topic,function(err){
+      console.log(ssd1406topic);
+      client.on('connect', function () {
+        console.log('connected');
 
-                if(!err){
-                    console.log('subscribed');
+        client.subscribe(ssd1306topic, function (err) {
 
-                     client.publish(ssd1406topic,key);
-                     res.status(200);
-                }
-                
-            })
-            });
-            client.on('message',function(topic,message){
-            console.log(message.toString());
-            const status=message.toString();
-            db.lockerData.findOneAndUpdate({name:req.body.name},{$set:{status:status}});
-            })
-            res.end();
-        } catch (error) {
-            console.log("Cant unlock",error);
-            res.sendstatus(500)
-        }
-    },
-  async newLocker(req,res){
-    try{
-    const user = await db.lockerData.findOne({name:req.body.name});
-    if(user)return res.status(400).send({error:"locker already exist"})
-    await db.lockerData.insertOne(req.body);
-    res.send({message:"locker added successfully"})
+          if (!err) {
+            console.log('subscribed');
 
-  }catch(err){
-    res.status(500)
-  }
-},
-async lockerdata(req,res){
-    try{
-    const data=await db.lockerData.find().toArray();
-    res.status(200).send(data);
+            client.publish(ssd1406topic, key);
+            res.status(200);
+          }
 
-  }catch(err){
-    res.status(500)
+        })
+      });
+      client.on('message', function (topic, message) {
+        console.log(message.toString());
+        const status = message.toString();
+        db.lockerData.findOneAndUpdate({ name: req.body.name }, { $set: { status: status } });
+      })
+      res.end();
+    } catch (error) {
+      console.log("Cant unlock", error);
+      res.sendstatus(500)
+    }
+  },
+  async newLocker(req, res) {
+    try {
+      const user = await db.lockerData.findOne({ name: req.body.name });
+      if (user) return res.status(400).send({ error: "locker already exist" })
+      await db.lockerData.insertOne(req.body);
+      res.send({ message: "locker added successfully" })
+
+    } catch (err) {
+      res.status(500)
+    }
+  },
+  async lockerdata(req, res) {
+    try {
+      const data = await db.lockerData.find().toArray();
+      res.status(200).send(data);
+
+    } catch (err) {
+      res.status(500)
+    }
+  },
+
+  async Updatedata(req, res) {
+    var status = "open"
+    try {
+      const data = await db.lockerData.findOneAndUpdate({ name: req.body.name }, { $set: { status: status } });
+      res.status(200).send(data);
+
+    } catch (err) {
+      res.status(500)
+    }
   }
 }
-}
 
-module.exports= service;
+module.exports = service;
