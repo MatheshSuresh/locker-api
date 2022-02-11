e0a7646243167c2093d70577acd248311596dc83
Dinesh Kumar <mdineshdk21@gmail.com>
2022-02-11 13:47:36 +0530

    google login added

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,5 +4,6 @@ const service=require("../services/user.services");
 
 route.post('/register',service.register);
 route.post('/login',service.login)
-
+route.get('/check',service.check)
+route.post('/update',service.update)
 module.exports = route;
--- services/user.services.js
+++ services/user.services.js
@@ -1,42 +1,63 @@
 const db = require("../mongo");
-const bcrypt=require("bcrypt");
+const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken');
 
 const service = {
-    async register(req,res) {
+    async register(req, res) {
         try {
-            const user = await db.userauth.findOne({email: req.body.email})
-            if(user)return res.status(400).send({error:"User already exist"})
-            
-            const salt= await bcrypt.genSalt(10);
-            req.body.password= await bcrypt.hash(req.body.password,salt);
+            const user = await db.userauth.findOne({ email: req.body.email })
+            if (user) return res.status(400).send({ error: "User already exist" })
+
+            const salt = await bcrypt.genSalt(10);
+            req.body.password = await bcrypt.hash(req.body.password, salt);
             await db.userauth.insertOne(req.body);
 
-            res.send({message:"user registered successfully"})
+            res.send({ message: "user registered successfully" })
             console.log("user registered successfully")
-            
+
         } catch (error) {
-            console.log("Error Registering User - ",error);
+            console.log("Error Registering User - ", error);
             res.sendstatus(500)
         }
     },
-    
-    async login(req,res){
+
+    async login(req, res) {
         try {
-            const user = await db.userauth.findOne({email: req.body.email})
-            if(!user)return res.status(400).send({error:"User not exist"})
+            const user = await db.userauth.findOne({ email: req.body.email })
+            if (!user) return res.status(400).send({ error: "User not exist" })
 
-            const isValid=await bcrypt.compare(req.body.password,user.password);
-            if(!isValid) return res.status(403).send({error:"Email or Password Not Exist"})
+            const isValid = await bcrypt.compare(req.body.password, user.password);
+            if (!isValid) return res.status(403).send({ error: "Email or Password Not Exist" })
 
-            const token= jwt.sign({userId:user._id,email:user.email},process.env.authpass,{expiresIn:'2h'})
-            res.header('auth',token).send(token);
-           
+            const token = jwt.sign({ userId: user._id, email: user.email }, process.env.authpass, { expiresIn: '2h' })
+            res.header('auth', token).send(token);
+
         } catch (error) {
-            console.log("Error login User - ",error);
+            console.log("Error login User - ", error);
             res.status(500);
-        } 
-    } 
+        }
+    },
+
+    async check(req, res) {
+        try {
+            const data = await db.userauth.find().toArray();
+            res.status(200).send(data);
+
+        } catch (err) {
+            res.status(500)
+        }
+    },
+
+    async update(req, res) {
+        const salt = await bcrypt.genSalt(10);
+        var status = await bcrypt.hash(req.body.password, salt);
+        try {
+          const data = await db.lockerData.findOneAndUpdate({ email: req.body.email }, { $set: { password: status } }).then((res) => { return res }).catch((err) => { throw err.message })
+          res.send(data);
+        } catch (err) {
+          res.status(500)
+        }
+      }
 }
 
 module.exports = service;
